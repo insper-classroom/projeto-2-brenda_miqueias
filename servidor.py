d81c95f9736341fc276e5a196293bb6fc3911175
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-03-13 10:50:55 -0300

    feat(api): route /imoveis

--- servidor.py
+++ servidor.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from utils import run_sql
 
 app = Flask(__name__)
+
+@app.route('/imoveis', methods=['GET'])
+def get_all_imoveis():
+    command = 'SELECT * FROM imoveis'
+    return run_sql(command, fetch=True)
 
 if __name__== '__main__':
     app.run(debug=True)
