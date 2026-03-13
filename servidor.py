4ebc6dd3b3aa0f8333ad5fd84475999913908898
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-03-13 12:10:00 -0300

    feat(api): rota /imoveis/id

--- servidor.py
+++ servidor.py
@@ -8,5 +8,10 @@ def get_all_imoveis():
     command = 'SELECT * FROM imoveis'
     return run_sql(command, fetch=True)
 
+@app.route('/imoveis/<int:id>', methods=['GET'])
+def get_imovel_id(id):
+    command = 'SELECT * FROM imoveis WHERE id=%s'
+    return run_sql(command, params=(id,), fetch=True)
+
 if __name__== '__main__':
     app.run(debug=True)
